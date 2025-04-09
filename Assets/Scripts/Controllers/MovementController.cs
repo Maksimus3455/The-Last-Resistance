5919715c41e723cc9720ed7c2ed12a55935c3af4
Maksimus3455 <maksimus3455@yandex.ru>
2025-04-09 13:01:58 +0300

    Library Controller
    Создание пробной библиотеки контроллера

--- Assets/Scripts/Controllers/MovementController.cs
+++ Assets/Scripts/Controllers/MovementController.cs
@@ -1,16 +1,7 @@
+using LibraryConrtollers;
 using UnityEngine;
 
-public class MovementController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //gfdgdfgdffdgfdg
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    MovementComponent mMovementComponent; 
 }
---
+++ Assets/Scripts/Libraries/LibraryConrtollers/MovementController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LibraryConrtollers
+{
+    internal class MovementComponent
+    {
+        private Camera Camera { get; set; } = Camera.main;
+        private void Movement(GameObject player, Vector2 InputDirection, Vector3 cameraDirection, float moveSpeed)
+        {
+            InputDirection.x = cameraDirection.x;
+            cameraDirection.y = 0;
+            InputDirection.y = cameraDirection.y;
+            var moveDir = new Vector3(cameraDirection.x, cameraDirection.y, cameraDirection.z);
+            var currentRot = player.transform.rotation;
+            var newRot = Quaternion.LookRotation(Vector3.Normalize(moveDir));
+            player.transform.rotation = Quaternion.Lerp(currentRot, newRot, Time.deltaTime * 20);
+            player.transform.position += moveSpeed * Time.deltaTime * moveDir;
+        }
+
+        public void PlayerMove(GameObject player, Vector2 InputDirection, float moveSpeed)
+        {
+            if (InputDirection.y > 0)
+                Movement(player, InputDirection, Camera.transform.forward, moveSpeed);
+
+            if (InputDirection.y < 0)
+                Movement(player, InputDirection, -Camera.transform.forward, moveSpeed);
+
+            if (InputDirection.x > 0)
+                Movement(player, InputDirection, Camera.transform.right, moveSpeed);
+
+            if (InputDirection.x < 0)
+                Movement(player, InputDirection, -Camera.transform.right, moveSpeed);
+        }
+    }
+}
